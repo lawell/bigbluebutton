1a25dcbeebbed059752c21dc7c3a57be865c9461
Stefan Riffert@Taz <riffert@la-well.de>
2015-06-10 10:25:50 +0200

    BBB Label aus js entfernt. Tabname ist nun auch korrekt

--- bigbluebutton-client/resources/prod/lib/bbb_blinker.js
+++ bigbluebutton-client/resources/prod/lib/bbb_blinker.js
@@ -1,5 +1,6 @@
 function setTitle(title){
-    document.title= "BigBlueButton - " + title;
+    //document.title= "BigBlueButton - " + title;
+    document.title= "ElVi";
 }
 
 function clientReady(message){
